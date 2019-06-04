dbb26bace954fa7b892eaa2b5f670fb6da8625d2
tournevis <arthur.ro92@gmail.com>
2019-06-05 00:42:52 +0200

    🐝 add database and duplicated check

--- server.js
+++ server.js
@@ -1,26 +1,15 @@
 const https = require('https')
-const baseUrl = "https://ws.louervite.fr/AnnonceWebService.svc/RechercherAnnonces?request="
+const searchParams = require('./config/search.json')
+const mysqlConf = require('./config/mysql.conf.json')
+const BASE_URL = "https://ws.louervite.fr/"
+const GET_NEW_FLAT = "AnnonceWebService.svc/RechercherAnnonces?request="
+const DB = require('./lib/db.js')
+const Flat = require('./lib/flat.js')
 
-var params = {
-  "Criteres": {
-    "CodesInsee": ["930048"],
-    "Tri": {
-      "Parameter": "DateCreation",
-      "Asc": false
-    },
-    "Pagination": {
-      "PageIndex": "0",
-      "PageSize": 20
-    },
-    "EstDerniereRecherche": true,
-    "EstRechercheRefine": false,
-    "TypesBien": [1],
-    "NbPiecesMin": 1,
-    "PrixMin": 100,
-    "PrixMax": 10000
-  }
-}
-var requestUrl = baseUrl + serialize(params)
+var database = new DB(mysqlConf)
+var requestUrl = BASE_URL + GET_NEW_FLAT + serialize(searchParams)
+
+
 https.get(requestUrl, res => {
   let data = ''
   res.on('data', (chunk) => {
@@ -42,5 +31,11 @@ function serialize (obj) {
 function  handleData(data) {
   var obj = JSON.parse(data)
   var r =  obj.d.Data.Resultats.Resultats
-  console.log(r)
+  console.log(`🏰 ${r.length} : Appartements trouvés`)
+  if (r) {
+    for (var i = r.length - 1; i >= 0; i--) {
+      var formatedFlat = Flat.serialize(r[i])
+      var t = database.add(formatedFlat)
+    }    
+  }
 }
